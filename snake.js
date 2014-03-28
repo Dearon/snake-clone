7de5f3654450012784d558c80c2e45cd5d1ea14c
Dearon <dearon@gmail.com>
2014-03-28 13:48:39 +0100

    Added collision with the edges of the level and the snake can no longer move in the opposite direction of the one it is currently traveling in

--- snake.js
+++ snake.js
@@ -1,34 +1,60 @@
+var width = 600;
+var height = 400;
 var direction = 'right';
 var speed = 2.5;
 
 var stage = new PIXI.Stage(0xFFFFFF);
-var renderer = PIXI.autoDetectRenderer(600, 400);
-
+var renderer = PIXI.autoDetectRenderer(width, height);
 document.body.appendChild(renderer.view);
-
-requestAnimFrame(animate);
 
 var snake = new PIXI.Graphics();
 snake.beginFill(0x000000);
-snake.drawCircle(200, 200, 10);
+snake.drawCircle(0, 0, 10);
 snake.endFill();
+
+snake.x = width/2;
+snake.y = height/2;
 stage.addChild(snake);
 
-function animate() {
-    requestAnimFrame(animate);
+main();
 
+function main() {
+    input();
+    movement();
+    collision();
+    requestAnimFrame(animate);
+}
+
+function animate() {
+    renderer.render(stage);
+    main();
+}
+
+function input() {
+    KeyboardJS.on('left', function() { 
+        if (direction != 'right') { direction = 'left'; }
+    });
+    KeyboardJS.on('right', function() {
+        if (direction != 'left') { direction = 'right'; }
+    });
+    KeyboardJS.on('up', function() {
+        if (direction != 'down') { direction = 'up'; }
+    });
+    KeyboardJS.on('down', function() {
+        if (direction != 'up') { direction = 'down'; }
+    });
+}
+
+function movement() {
     if (direction == 'left') { snake.x -= speed; }
     if (direction == 'right') { snake.x += speed; }
     if (direction == 'up') { snake.y -= speed; }
     if (direction == 'down') { snake.y += speed; }
-
-    handleInput();
-    renderer.render(stage);
 }
 
-function handleInput() {
-    KeyboardJS.on('left', function() { direction = 'left'; });
-    KeyboardJS.on('right', function() { direction = 'right'; });
-    KeyboardJS.on('up', function() { direction = 'up'; });
-    KeyboardJS.on('down', function() { direction = 'down'; });
+function collision() {
+    if (snake.x < 0 || snake.x > width || snake.y < 0 || snake.y > height) {
+        snake.x = width/2;
+        snake.y = height/2;
+    }
 }
